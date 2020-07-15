1cdb0652972d2394e6ce2469015028eaac99f46e
duynguyen <duynguyen0444@gmail.com>
2020-07-15 21:52:08 +0700

    config port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 //console.log(process.env.SENDGRID_API_KEY);
 
 // url https://api.sendgrid.com/v3/mail/send \
@@ -6,62 +6,62 @@ require("dotenv").config();
 // header 'content-type: application/json'
 // data '{"personalizations":[{"to":[{"email":"john.doe@example.com","name":"John Doe"}],"subject":"Hello, World!"}],"content": [{"type": "text/plain", "value": "Heya!"}],"from":{"email":"sam.smith@example.com","name":"Sam Smith"},"reply_to":{"email":"sam.smith@example.com","name":"Sam Smith"}}'
 
-var express = require("express");
+var express = require('express');
 var app = express();
-var port = 3000;
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var mongoose = require("mongoose");
+var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URL);
 
-var bookRouter = require("./routes/book.route");
-var userRouter = require("./routes/user.route");
-var transactionRouter = require("./routes/transactions.route");
-var authenRouter = require("./routes/authen.route");
+var bookRouter = require('./routes/book.route');
+var userRouter = require('./routes/user.route');
+var transactionRouter = require('./routes/transactions.route');
+var authenRouter = require('./routes/authen.route');
 
-var authenMiddleware = require("./middleware/authen.middleware");
-var adminMiddleware = require("./middleware/admin.middleware");
-var sessionMiddleware = require("./middleware/session.middleware");
+var authenMiddleware = require('./middleware/authen.middleware');
+var adminMiddleware = require('./middleware/admin.middleware');
+var sessionMiddleware = require('./middleware/session.middleware');
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.set('view engine', 'pug');
+app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(cookieParser(process.env.SENDGRID_API_KEY));
 app.use(sessionMiddleware);
 
 //let count = 0;
-app.get("/", (req, res) => {
-  // res.cookie("cookie-id", 1234);
-  // if (req.cookies) {
-  //   count += 1;
-  // }
-  // console.log(`<cookie>: ${count}`);
-  res.render("index");
+app.get('/', (req, res) => {
+	// res.cookie("cookie-id", 1234);
+	// if (req.cookies) {
+	//   count += 1;
+	// }
+	// console.log(`<cookie>: ${count}`);
+	res.render('index');
 });
 
 app.use(
-  "/users",
-  authenMiddleware.requireAuthen,
-  //adminMiddleware.requireRole(true),
-  userRouter
+	'/users',
+	authenMiddleware.requireAuthen,
+	//adminMiddleware.requireRole(true),
+	userRouter
 );
 app.use(
-  "/books",
-  //authenMiddleware.requireAuthen,
-  // adminMiddleware.requireRole(true),
-  bookRouter
+	'/books',
+	//authenMiddleware.requireAuthen,
+	// adminMiddleware.requireRole(true),
+	bookRouter
 );
 app.use(
-  "/transactions",
-  authenMiddleware.requireAuthen,
-  //adminMiddleware.requireRole(true),
-  transactionRouter
+	'/transactions',
+	authenMiddleware.requireAuthen,
+	//adminMiddleware.requireRole(true),
+	transactionRouter
 );
-app.use("/authen", authenRouter);
+app.use('/authen', authenRouter);
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+	console.log(`Server listening on port ${port}`);
 });
